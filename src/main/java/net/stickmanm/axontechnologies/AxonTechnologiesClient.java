0ca16dc31806929d88974c72f27ec4afe3b779c0
StickManM <deapen00@outlook.com>
2024-08-31 18:18:50 -0500

    Initial commit

---
+++ src/main/java/net/stickmanm/axontechnologies/AxonTechnologiesClient.java
@@ -0,0 +1,12 @@
+package net.stickmanm.axontechnologies;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AxonTechnologiesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
+
---
+++ src/main/java/net/stickmanm/axontechnologies/AxonTechnologiesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.stickmanm.axontechnologies;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AxonTechnologiesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
